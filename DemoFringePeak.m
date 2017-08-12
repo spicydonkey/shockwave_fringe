282eb98d157c5b839e2d9b474e3db9cd4012f8b4
David <dk.shin1992@gmail.com>
2017-08-12 18:09:12 +1000

    tidy codebase: var names updated and tidied

--- DemoFringePeak.m
+++ DemoFringePeak.m
@@ -1,19 +1,21 @@
 %% Fringe characterisation by peak spacing through line profile
+% 
 
 %%% configure
-% load line profile
-d_1d=zrot_c(id_y);     % distance along line
-
 % background smoothing
 n_sm_bgd=15;
 n_sm_post=10;
 
 % peak detection algorithm
-SlopeThreshold=1e-4;
-AmpThreshold=0.1;
+slopethreshold=1e-4;
+ampthreshold=0.1;
 smoothwidth=0;          % 0 to no smoothing in peak finding
 peakgroup=5;
 smoothtype=1;   % smoothing is off
+
+%% main
+% load line profile
+d_1d=zz;     % distance along line
 
 % preallocate
 peak_list=cell(pal_nseq,1);     % full peak summary output from findpeaksG
@@ -21,33 +23,42 @@ ppeak=cell(pal_nseq,1);         % peak locations for each PAL [mm]
 dppeak=cell(pal_nseq,1);        % peak spacing [mm]
 
 hfig_peaks=figure();
-cc=distinguishable_colors(pal_nseq);        % for plotting per PAL #
 
-%% preprocess fringes
-% background subtraction
-n1d_bgd=cellfun(@(x) smooth(x,n_sm_bgd),nn1d,'UniformOutput',false);    % background from smoothing - moving average
-dn1d=cellfun(@(x,y) x-y,nn1d,n1d_bgd,'UniformOutput',false);        % background subtracted density profile
+%% preprocess fringes for peak detection
+% background subtraction - moving average
+n1d_bgd=cellfun(@(x) smooth(x,n_sm_bgd),nn1d,'UniformOutput',false);
+
+% background subtracted density profile
+dn1d=cellfun(@(x,y) x-y,nn1d,n1d_bgd,'UniformOutput',false);        
+
 % additional smoothing on diff density fringes
 dn1d=cellfun(@(x) smooth(x,n_sm_post),dn1d,'UniformOutput',false);
 
 %% Find peaks
 p=zeros(10,1);  % array to store figure objects for selective legend
 for ii=1:pal_nseq
-    n=dn1d{ii};         % 1D density profile along line
-    n=n/max(n);         % normalise density; max=1
+    % normalise density fringe
+    n=dn1d{ii};
+    n=n/max(n);         % normalise to max=1
     
-    this_peak_list=findpeaksG(1e3*d_1d,n,SlopeThreshold,AmpThreshold,smoothwidth,peakgroup,smoothtype);
-    peak_list{ii}=this_peak_list;
+    % find peaks
+    this_peak_list=findpeaksG(1e3*d_1d,n,slopethreshold,ampthreshold,smoothwidth,peakgroup,smoothtype);
+    peak_list{ii}=this_peak_list;   % store the peaks in cell array
     
+    %%% plot result
     figure(hfig_peaks);
     hold on;
+    % processed density fluctuations
     p(ii)=plot(1e3*d_1d,n,'color',cc(ii,:),'LineWidth',1.5,'DisplayName',sprintf('%d',ii));
+    
+    % detected peaks on profile
     scatter(this_peak_list(:,2),this_peak_list(:,3),'o','MarkerEdgeColor',cc(ii,:));
     text(this_peak_list(:,2),this_peak_list(:,3),num2str(this_peak_list(:,1)),...
         'Color',cc(ii,:),'FontSize',15);  % annotate peaks
     
+    % store peak positions summary
     ppeak{ii}=this_peak_list(:,2)';
-    dppeak{ii}=diff(ppeak{ii});
+    dppeak{ii}=diff(ppeak{ii});         % peak spacing [m]
 end
 % annotate figures
 figure(hfig_peaks);
@@ -57,9 +68,9 @@ ylabel('density fluctuation from moving average [arb]');
 lgd=legend(p);
 title(lgd,'PAL');
 
-% collate peak data
+% collate peak data in array
 max_peak_n=max(cellfun(@(x)size(x,2),ppeak));       % max number of peaks found in PALs
-peak_pos=NaN(pal_nseq,max_peak_n);  % preallocate NaN array
+peak_pos=NaN(pal_nseq,max_peak_n);                  % preallocate NaN array
 for ii=1:pal_nseq
     this_peak_pos=ppeak{ii};
     peak_pos(ii,1:size(this_peak_pos,2))=this_peak_pos;
@@ -68,33 +79,32 @@ peak_diff=diff(peak_pos,1,2);   % evaluate peak spacing
 
 cc2=distinguishable_colors(max_peak_n-1);   % for plotting per FRINGE #
 
-%%% plot peak spacings
-% peak narrowing
-hfig_dppeaks_vs_n=figure();
-hold on;
-for ii=1:pal_nseq
-    plot(dppeak{ii},'o','color',cc(ii,:));
-end
-box on;
-xlabel('$n$');
-ylabel('$n$-th peak spacing [mm]');
-
-% peak spacing dependence on PAL
-hfig_dppeaks_vs_pal=figure();
-max_peak_n=max(cellfun(@(x)size(x,2),ppeak));       % max number of peaks found in PALs
-for ipeak=1:max_peak_n-1
-    subplot(max_peak_n-1,1,ipeak);
-    xlim([1,pal_nseq]);
-    hold on;
-    box on;
-    title(sprintf('peak spacing: %d',ipeak));
-    % improve by padding with NaN, etc.
-    for ii=1:pal_nseq
-        if length(dppeak{ii})>=ipeak
-            scatter(ii,dppeak{ii}(ipeak),'MarkerEdgeColor',cc(ii,:));
-        end
-    end
-end
+% %%% plot peak spacings
+% % peak narrowing
+% hfig_dppeaks_vs_n=figure();
+% hold on;
+% for ii=1:pal_nseq
+%     plot(dppeak{ii},'o','color',cc(ii,:));
+% end
+% box on;
+% xlabel('$n$');
+% ylabel('$n$-th peak spacing [mm]');
+% 
+% % peak spacing dependence on PAL
+% hfig_dppeaks_vs_pal=figure();
+% max_peak_n=max(cellfun(@(x)size(x,2),ppeak));       % max number of peaks found in PALs
+% for ipeak=1:max_peak_n-1
+%     subplot(max_peak_n-1,1,ipeak);
+%     xlim([1,pal_nseq]);
+%     hold on;
+%     box on;
+%     title(sprintf('peak spacing: %d',ipeak));
+%     for ii=1:pal_nseq
+%         if length(dppeak{ii})>=ipeak
+%             scatter(ii,dppeak{ii}(ipeak),'MarkerEdgeColor',cc(ii,:));
+%         end
+%     end
+% end
 
 %% peak spacing dependance on PAL population
 linewidth=1.5;
@@ -104,10 +114,7 @@ valarray={linewidth,'w'};                 % 90 deg (normal) data
 hfig_dpeak_vs_Npal=figure();
 p=zeros(1,(max_peak_n-1));      % array to store figure objects for selective legend
 for ii=1:(max_peak_n-1)
-    hold on;
-%     plot(Nal,peak_diff(:,ii),...
-%         'o','Color',cc(ii,:),'DisplayName',sprintf('%d',ii));
-    
+    hold on;    
     hdata_pal_n=ploterr(Nal,peak_diff(:,ii),pal_n(:,2),[],'o','hhxy',0);
     set(hdata_pal_n(1),namearray,valarray,'Color',cc2(ii,:),'DisplayName',sprintf('%d',ii));
     set(hdata_pal_n(2),namearray,valarray,'Color',cc2(ii,:),'DisplayName','');
@@ -115,7 +122,6 @@ for ii=1:(max_peak_n-1)
 end
 box on;
 lgd=legend(p);
-% lgd=legend('show');
 title(lgd,'Fringe spacing');
 xlabel('$N_{AL}$');
 ylabel('Fringe spacing [mm]');
--- main_process_pal.m
+++ main_process_pal.m
@@ -1,66 +1,90 @@
 %% Process PAL to analyse 1D density fringes through the jet
-%%% configure
+% transforms 3D translation (YZ) + rotation operation (X) to align fringes
+% along vertical (Z') direction. take 2D density profile in Y'Z'
+% projection (integrates X) - take finite width line profile along Z' to
+% obtain 1d density fringes.
+
+%% configure
+% configure the 1d density profile
 fringe_cfg.offset=[0,-0.006];       % yz translation for centering transformation
 fringe_cfg.theta=1.1;               % rotation angle around x-axis (rad) - to align fringes in vertical Y-axis
-fringe_cfg.width=2e-3;            % 1d profile perpendicularly [m]
+fringe_cfg.width=2e-3;              % 1d profile perpendicularly [m]
+fringe_cfg.dlim=[0,20e-3];          % line profile limits
+
+% density - histogramming (in rotated axis)
+yy_ed=edges{3};
+zz_ed=edges{1};
+yy_c=cents{3};
+zz_c=cents{1};
+
+nsmooth_1d_raw=5;   % moving average sample size - smoothing raw 1D profile
+
+% plotting
+plot_ncol=ceil(sqrt(pal_nseq));         % subplot num cols
+plot_nrow=ceil(pal_nseq/plot_ncol);     % subplot num rows
+
+cc=distinguishable_colors(pal_nseq);	% colors for plotting against PAL properties
+
+%% main
+% get indices for data in fringe ROI
+id_yy=find(abs(yy_c)<fringe_cfg.width);       % perpendicular indices to integrate over
+id_zz=find(zz_c>=fringe_cfg.dlim(1)&zz_c<fringe_cfg.dlim(2));    % y-indices ROI
+
+% get pos vectors for ROI
+yy=yy_c(id_yy);
+zz=zz_c(id_zz);
+
+% build ROI position for 'rectangle' graphical annotation
+roi_x=min(yy);
+roi_y=min(zz);
+roi_w=diff(minmax(yy));
+roi_h=diff(minmax(zz));
+pos_roi=[roi_x,roi_y,roi_w,roi_h];
 
 % preallocate
 nn1d=cell(pal_nseq,1);      % 1D density profile
 
 hfig_ndenrot=figure();
 hfig_ndenraw=figure();  
-plot_ncol=ceil(sqrt(pal_nseq));         % configure subplots layout
-plot_nrow=ceil(pal_nseq/plot_ncol);
 hfig_nden1d=figure();
-cc=distinguishable_colors(pal_nseq);        % colors
+
 for pal_id=1:pal_nseq
     % variable 'pal' is Nx3 array
     pal=vertcat(pal_zxy0{pal_id}{:});    % collate all shots in this PAL
     
     %%% transform PAL zxy to align jet to Z-axis
     % cull X --> unused
-    pal=pal(:,[3,1]);   % YZ
+    pal=pal(:,[3,1]);   % Y'Z'
     
-    % translate in TY plane - centre to jet crossing
+    % translate in ZY plane - centre to jet crossing
     pal=pal+repmat(fringe_cfg.offset,[size(pal,1),1]);
     
     % rotate about X-axis
-    pal_xrot=pal;   % preallocate
-    pal_xrot(:,1)=cos(fringe_cfg.theta)*pal(:,1)-sin(fringe_cfg.theta)*pal(:,2);
-    pal_xrot(:,2)=sin(fringe_cfg.theta)*pal(:,1)+cos(fringe_cfg.theta)*pal(:,2);
+    pal_rot=pal;   % preallocate
+    pal_rot(:,1)=cos(fringe_cfg.theta)*pal(:,1)-sin(fringe_cfg.theta)*pal(:,2);
+    pal_rot(:,2)=sin(fringe_cfg.theta)*pal(:,1)+cos(fringe_cfg.theta)*pal(:,2);
     
     %%% evaluate 2D density
-    yrot_ed=edges{3};
-    zrot_ed=edges{1};
-    yrot_c=cents{3};
-    zrot_c=cents{1};
+    nn2d=density2d(pal_rot,{yy_ed,zz_ed})';
     
-    nn=density2d(pal_xrot,{yrot_ed,zrot_ed})';      % needs to be transposed
-    
-    % plot 2D projected density profile
+    % plot full 2D projected density profile
     figure(hfig_ndenrot);
     subplot(plot_nrow,plot_ncol,pal_id);
-    imagesc(yrot_c,zrot_c,nn);
+    imagesc(yy_c,zz_c,nn2d);
     set(gca,'YDir','normal');
     axis equal; axis tight; 
     title(sprintf('%d',pal_id));
     
-    %%% take line profile of the density thru Y-axis at X=0
-    [~,id_x0]=min(abs(yrot_c));
-    
-    id_x=find(abs(yrot_c)<fringe_cfg.width);       % perpendicular indices to integrate over
-    id_y=find(zrot_c>=0&zrot_c<0.02);           % y-indices ROI
-    
     %%% prepare 1D density profile through shockwave
-    nn_raw=nn(id_y,id_x);            % raw density in region of interest
-    nn1d_temp=mean(nn_raw,2);        % integrate thru perpendicular dir
-    nn1d{pal_id}=smooth(nn1d_temp,5);      % simple smoothing - moving average
+    nn_raw=nn2d(id_zz,id_yy);                 % raw 2d density in region of interest
+    nn1d_temp=mean(nn_raw,2);               % integrate thru perpendicular dir
+    nn1d{pal_id}=smooth(nn1d_temp,nsmooth_1d_raw);       % simple smoothing - moving average
     
     %%% plot result
-    % raw density in region of interest
+    % 2D density in 2D region of interest
     figure(hfig_ndenraw);
     subplot(plot_nrow,plot_ncol,pal_id);
-    imagesc(yrot_c(id_x),zrot_c(id_y),nn_raw);
+    imagesc(yy,zz,nn_raw);
     set(gca,'YDir','normal');
     title(sprintf('%d',pal_id));
     
@@ -68,17 +92,12 @@ for pal_id=1:pal_nseq
     figure(hfig_ndenrot);
     subplot(plot_nrow,plot_ncol,pal_id);
     hold on;
-    roi_x=min(yrot_c(id_x));    % rectange bottom left corner
-    roi_y=min(zrot_c(id_y));
-    roi_w=diff(minmax(yrot_c(id_x)));
-    roi_h=diff(minmax(zrot_c(id_y)));
-    pos_roi=[roi_x,roi_y,roi_w,roi_h];      % draw rectangle
     rectangle('Position',pos_roi,'EdgeColor','w');
     
     % 1D density profile
     figure(hfig_nden1d);
     hold on;
-    plot(1e3*zrot_c(id_y),nn1d{pal_id},...
+    plot(1e3 *zz,nn1d{pal_id},...
         'DisplayName',sprintf('%d: %0.2g, %0.2g',pal_id,Nal(pal_id),N0(pal_id)),...
         'LineWidth',1.5,'color',cc(pal_id,:));
 end
